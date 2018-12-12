daf6aef859daffaaefbb7172bdb3db02f43a17c4
Orkhan <gasanov.orkhan1@gmail.com>
2018-12-12 09:18:31 -0500

    Comparison of sorting in Java7 and Java8

---
+++ src/main/java/org/orkhan/gasanov/java8/functional/programming/sample/User.java
@@ -0,0 +1,51 @@
+package org.orkhan.gasanov.java8.functional.programming.sample;
+
+/**
+ * Created by Orkhan Gasanov on December 12, 2018
+ */
+
+public class User {
+
+    private String firstName;
+    private String secondName;
+    private  int age;
+
+    public User(String firstName, String secondName, int age) {
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "firstName='" + firstName + '\'' +
+                ", secondName='" + secondName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
