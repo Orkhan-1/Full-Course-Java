6cde9437469f6c289650b0ecc067634ae11202ab
Orkhan <gasanov.orkhan1@gmail.com>
2025-09-06 23:21:24 -0700

    [Tutorial 1] equals and hashCode

---
+++ equals/Book.java
@@ -0,0 +1,33 @@
+import java.util.Objects;
+
+public class Book extends Object {
+    public String title;
+    public String author;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+
+        Book otherBook = (Book) obj;
+
+        return title.equals(otherBook.title) &&
+                author.equals(otherBook.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(title, author);
+    }
+
+}
---
+++ equals/Main.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Book book1 = new Book("Advanced Java", "Orkhan G.");
+        Book book2 = new Book("Advanced Java", "Orkhan G.");
+
+        System.out.println(book1.equals(book2));
+
+        Set<Book> bookShelf = new HashSet<>();
+        bookShelf.add(book1);
+
+        System.out.println(bookShelf.contains(book2));
+
+    }
+}
