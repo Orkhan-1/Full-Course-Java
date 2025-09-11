038a4b447fa7ad512e6af452eb6a383ab55a4daa
Orkhan <gasanov.orkhan1@gmail.com>
2025-09-10 18:03:08 -0700

    [Tutorial 2] while vs do-while

---
+++ src/tutorial_2/DoWhileExample.java
@@ -0,0 +1,12 @@
+package tutorial_2;
+
+public class DoWhileExample {
+    public static void main(String[] args) {
+        int i = 1;
+
+        do {
+            System.out.println("Do-while loop: " + i);
+            i++;
+        } while (i < 10);
+    }
+}
