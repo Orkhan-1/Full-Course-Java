2c0e52ca5255e2104c29f49f339c09c194723787
Orkhan <gasanov.orkhan1@gmail.com>
2025-09-14 23:24:13 -0700

    [Tutorial 3, 4, 5, 6]

--- src/tutorial_6/DemoApp.java
+++ src/tutorial_6/DemoApp.java
@@ -1,15 +1,11 @@
 package tutorial_6;
 
-import org.apache.commons.lang3.StringUtils;
+//import org.apache.commons.lang3.StringUtils;
 
 public class DemoApp {
     public static void main(String[] args) {
         String name = "hello world from apache commons lang3";
-        String capitalized = StringUtils.capitalize(name);
-        System.out.println("Capitalized: " + capitalized);
+        //String capitalized = StringUtils.capitalize(name);
+        //System.out.println("Capitalized: " + capitalized);
     }
 }
-
-// javac -cp "lib/commons-lang3-3.18.0.jar" DemoApp.java -d bin
-// java -cp "bin:lib/commons-lang3-3.18.0.jar" tutorial_6.DemoApp
-
